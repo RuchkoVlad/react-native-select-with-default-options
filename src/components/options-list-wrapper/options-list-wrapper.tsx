328b3990653a6258c455bb0ef2328aa76540e523
Vladislav <necessarydp@gmail.com>
2024-05-16 12:44:05 +0300

    Remove fixed height

--- src/components/options-list-wrapper/options-list-wrapper.tsx
+++ src/components/options-list-wrapper/options-list-wrapper.tsx
@@ -60,7 +60,7 @@ const styles = StyleSheet.create<Styles>({
         zIndex: 1,
         backgroundColor: COLORS.WHITE,
         borderWidth: BORDER_WIDTH,
-        maxHeight: OPTIONS_LIST_HEIGHT,
+        // maxHeight: OPTIONS_LIST_HEIGHT,
         elevation: 5,
     },
     notOverflown: {
